a4b69e97a39e27609c7bc42dd6f03826c26db2aa
showmittro <showmittrobiswas2000@gmail.com>
2022-05-13 23:55:12 +0600

    about section edit and details

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -8,16 +8,20 @@ const About = () => {
                 <h1 className='pt-5'>About Me</h1>
                 <div className="row align-items-center pt-3">
                     <div className="col-12 col-md-6">
-                        <img style={{height:'400px'}} src={MePic} className="" alt="" />
+                        <img style={{ height: '400px' }} src={MePic} className="" alt="" />
 
                     </div>
                     <div className="col-12 col-md-6">
                         <h3>Showmittro Biswas</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi voluptas ut placeat quas consectetur dolores qui, distinctio velit maxime itaque ab praesentium officiis modi eius esse blanditiis vel voluptatem. Quo expedita dolorem fuga eaque est libero quasi ipsum! Molestiae illo hic, et error quos nemo quo ducimus? Possimus, dolorum quibusdam!</p>
+                        <p>Hey,,
+                            I am Showmittro Biswas. I am a student of Feni Computer Institute of Data Telecommunication and Networking Technology. <br /><br />
+                            I am a professional Front End Web developer
+                            I’m a Front End Web developer who is passionate about making error-free websites with 100% client satisfaction. I love to solve real-world problems. I am strategic, goal-oriented, and always work with an end goal in mind. I pride myself on doing quality work and maintaining excellent communication. Most of the time I work with JavaScript ReactJS.
+                        </p>
                     </div>
                 </div>
             </div>
-            
+
         </div>
     );
 };
